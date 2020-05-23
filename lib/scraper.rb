bf243143749ab788cadd103a1a8f4680f54e758a
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-23 18:03:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,16 +4,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open(https://learn-co-curriculum.github.io/student-scraper-test-page/index.html)
-    binding.pry
-    doc.css("div.roster-cards-container").each do |student|
-      students = {
-        :name => student.css("h4.studen-name").text
-        :location => student.css("p.student-location").text
-        :profile_url => student.css("a").attribute("href").value
-      }
-    end
-    students
+    
   end
 
   def self.scrape_profile_page(profile_url)
