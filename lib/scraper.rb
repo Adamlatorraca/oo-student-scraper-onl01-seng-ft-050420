fd830ad5d1c19b582eb21401a6a0c68f040c6216
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-23 18:59:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,6 +29,7 @@ class Scraper
         :location => student.css("p.student-location").text,
         :profile_url => student.css("a").attribute("href").value
       }
+      students << student_info
     end
     binding.pry
     students
