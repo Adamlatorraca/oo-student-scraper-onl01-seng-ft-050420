8b6018a539d9f18f5dfe5cb9a6984b2d1d350102
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-23 18:10:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,11 +6,13 @@ class Scraper
 =begin  def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(https://learn-co-curriculum.github.io/student-scraper-test-page/index.html)
     binding.pry
+    students {}
     doc.css("div.roster-cards-container").each do |student|
+      student = student.css(""div.roster-cards-container"").text
       students = {
         :name => student.css("h4.studen-name").text
         :location => student.css("p.student-location").text
-        :profile_url => student.css("a").attribute("href").value
+        :profile_url => student.css("a").attribute("href").text
       }
     end
     students
@@ -25,7 +27,7 @@ def self.scrape_index_page(index_url)
       student_details = {}
       student_details[:name] = student.css("h4.student-name").text
       student_details[:location] = student.css("p.student-location").text
-      profile_path = student.css("a").attribute("href").value
+      profile_path = student.css("a").attribute("href").text
       student_details[:profile_url] = './fixtures/student-site/' + profile_path
       students << student_details
     end
