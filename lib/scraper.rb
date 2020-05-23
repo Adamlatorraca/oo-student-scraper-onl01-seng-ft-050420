2cab221e275d3600952004cb8d7cc36135b856d3
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-23 17:58:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,12 @@ class Scraper
     students = {}
     binding.pry
     doc.css("div.roster-cards-container").each do |student|
-      name = student.css("h4.student-name").text
+      students = {
+        :name => student.css("h4.studen-name").text
+        :location => student.css("p.student-location").text
+        :profile_url => student.css("a").attribute("href").value
+      }
+
     end
   end
 
