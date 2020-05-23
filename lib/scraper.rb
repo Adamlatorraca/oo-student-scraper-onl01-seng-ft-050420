5282c23b7bb7926cf1e6f5c00ddded95b1635a83
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-23 17:38:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(https://learn-co-curriculum.github.io/student-scraper-test-page/index.html)
     students = {}
-    doc.css(div.roster-cards-container)
+    doc.css("div.roster-cards-container")
   end
 
   def self.scrape_profile_page(profile_url)
