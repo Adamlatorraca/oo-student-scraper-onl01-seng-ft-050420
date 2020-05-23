9f372f525e9805802794326fa07ca98a56cbe2fa
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-23 18:22:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,6 +21,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     binding.pry
     html = open(index_url)
+    doc = Nokogiri::HTML(html)
   end
 
   def self.scrape_profile_page(profile_url)
